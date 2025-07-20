b8bb58c7916457f801e2985b2d6f2732d1233035
togunchan <togunchan@gmail.com>
2025-07-20 23:40:42 +0300

    test(minidb): add exportToJson() unit test and fix IntelliSense config

--- CppMiniDB/tests/test_minidb.cpp
+++ CppMiniDB/tests/test_minidb.cpp
@@ -1,3 +1,19 @@
-int main() {
-    return 0;
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include "cppminidb/MiniDB.hpp"
+
+TEST_CASE("MiniDB basic insert and export", "[MiniDB]")
+{
+    MiniDB db("test_table");
+    db.setColumns({"name", "age"});
+
+    db.insertRow({"Alice", "30"});
+    db.insertRow({"Bob", "25"});
+
+    std::string json = db.exportToJson();
+
+    REQUIRE(json.find("Alice") != std::string::npos);
+    REQUIRE(json.find("30") != std::string::npos);
+    REQUIRE(json.find("Bob") != std::string::npos);
+    REQUIRE(json.find("25") != std::string::npos);
 }
