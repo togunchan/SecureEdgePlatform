71a60b9823bfdf4028a5e6ad98e75fc3ac1645d3
togunchan <togunchan@gmail.com>
2025-07-21 23:37:29 +0300

    test(minidb): add tests for row insertion errors and structured data loading

--- CppMiniDB/tests/test_minidb.cpp
+++ CppMiniDB/tests/test_minidb.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 #include "cppminidb/MiniDB.hpp"
+#include <iostream>
 
 TEST_CASE("MiniDB basic insert and export", "[MiniDB]")
 {
@@ -16,4 +17,33 @@ TEST_CASE("MiniDB basic insert and export", "[MiniDB]")
     REQUIRE(json.find("30") != std::string::npos);
     REQUIRE(json.find("Bob") != std::string::npos);
     REQUIRE(json.find("25") != std::string::npos);
+}
+
+TEST_CASE("MiniDB throws on mismatched row insert", "[MiniDB]")
+{
+    MiniDB db("fail_table");
+    db.setColumns({"id", "name"});
+
+    REQUIRE_THROWS_AS(db.insertRow({"only_one_value"}), std::invalid_argument);
+}
+
+TEST_CASE("MiniDB selectAll returns structured data", "[MiniDB]")
+{
+    MiniDB db("select_table");
+    db.setColumns({"city", "temperature"});
+
+    db.insertRow({"Istanbul", "29"});
+    db.insertRow({"Ankara", "25"});
+    db.save();
+
+    auto results = db.loadFromDisk();
+
+    std::cout << "Row count: " << db.loadFromDisk().size() << std::endl;
+    REQUIRE(results.size() == 2);
+
+    REQUIRE(results[0]["city"] == "Istanbul");
+    REQUIRE(results[0]["temperature"] == "29");
+
+    REQUIRE(results[1]["city"] == "Ankara");
+    REQUIRE(results[1]["temperature"] == "25");
 }
